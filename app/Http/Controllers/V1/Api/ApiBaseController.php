12de215342ed8c5ffbb75f9bd5281a23381da8dc
alikeshtkar <alikeshtkar262@gmail.com>
2022-12-01 14:51:57 +0330

    fix problem swagger cros

--- app/Http/Controllers/V1/Api/ApiBaseController.php
+++ app/Http/Controllers/V1/Api/ApiBaseController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use OpenApi\Annotations as OA;
 
 /**
- * @OA\Server(url="http://127.0.0.1:8000/api/v1")
  * @OA\Info(
  *     version="1.0",
  *     title="Nika Web Services"
