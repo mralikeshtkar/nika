fc9bc7c45d1bda71471b9996a3562a7bbc87af2f
alikeshtkar <alikeshtkar262@gmail.com>
2022-12-21 10:52:14 +0330

    fix problem user resource 13

--- app/Http/Resources/V1/User/UserResource.php
+++ app/Http/Resources/V1/User/UserResource.php
@@ -18,7 +18,7 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return collect($this->resource)->when($this->resource->status && UserStatus::hasValue(intval($this->resource->status)), function (Collection $collection) {
+        return collect($this->resource)->when(!is_null($this->resource->status), function (Collection $collection) {
             $status = UserStatus::coerce(intval($this->resource->status));
             $collection->put('status', $status->key)->put('status_translated', $status->description);
         })->when($this->resource->originalIsEquivalent('birthdate'), function (Collection $collection) {
