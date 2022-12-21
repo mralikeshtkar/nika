43cf014fef1269f3a253a3111fd9cfbb0b814eed
alikeshtkar <alikeshtkar262@gmail.com>
2022-12-21 10:43:24 +0330

    fix problem user resource 4

--- app/Http/Resources/V1/User/UserResource.php
+++ app/Http/Resources/V1/User/UserResource.php
@@ -27,6 +27,10 @@ class UserResource extends JsonResource
             $collection->put('grade_name', $this->resource->grade_name);
         })->when($this->resource->originalIsEquivalent('city_name'), function (Collection $collection) {
             $collection->put('city_name', $this->resource->city_name);
+        })->when($this->resource->relationLoaded('province'), function (Collection $collection) {
+            $collection->put('province', $this->resource->province);
+        })->when($this->resource->originalIsEquivalent('birth_place_name'), function (Collection $collection) {
+            $collection->put('birth_place_name', $this->resource->birth_place_name);
         })->toArray();
     }
 }
