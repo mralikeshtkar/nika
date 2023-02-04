50b5f9659b84af77faa385f59b78397b8cbd41c7
alikeshtkar <alikeshtkar262@gmail.com>
2023-02-05 00:26:53 +0330

    push

--- app/Repositories/V1/User/Eloquent/UserRepository.php
+++ app/Repositories/V1/User/Eloquent/UserRepository.php
@@ -167,8 +167,11 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
     public function assignRahjooRole($user)
     {
         if ($user->wasRecentlyCreated) {
+            dd("حم");
             $user->syncRoles(Role::RAHJOO);
-            resolve(RahjooRepositoryInterface::class)->create([
+            resolve(RahjooRepositoryInterface::class)->updateOrCreate([
+                'user_id' => $user->id,
+            ], [
                 'user_id' => $user->id,
             ]);
         }
