8263de35ccde678888901f3ad31c1e282e37350c
alikeshtkar <alikeshtkar262@gmail.com>
2023-02-04 14:41:28 +0330

    push

--- app/Repositories/V1/Package/Eloquent/PackageRepository.php
+++ app/Repositories/V1/Package/Eloquent/PackageRepository.php
@@ -206,7 +206,7 @@ class PackageRepository extends BaseRepository implements PackageRepositoryInter
      */
     public function pivotIntelligencePackageHasExercise(Request $request, $exercise_ids): static
     {
-        $this->model = $this->model->withWhereHas('pivotIntelligencePackage', function ($q) use ($request, $exercise_ids) {
+        $this->model = $this->model->with('pivotIntelligencePackage', function ($q) use ($request, $exercise_ids) {
             $q->withWhereHas('exercise', function ($q) use ($request, $exercise_ids) {
                 $q->select(['id', 'intelligence_package_id', 'title'])
                     ->whereNotIn('id', $exercise_ids->toArray())
