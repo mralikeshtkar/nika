568b6c868bb50a93829320ed98aa49a2f8772e92
alikeshtkar <alikeshtkar262@gmail.com>
2023-02-08 02:12:44 +0330

    push

--- app/Repositories/V1/Package/Eloquent/PackageRepository.php
+++ app/Repositories/V1/Package/Eloquent/PackageRepository.php
@@ -165,9 +165,9 @@ class PackageRepository extends BaseRepository implements PackageRepositoryInter
         /** @var Package $package */
         return $package->exercises()
             ->has('questions')
-            ->withCount(['questionAnswerTypes','questionAnswersRahjoos'])
-            ->withQuestionAnswersCount($rahjoo->id)
-            ->orderByRaw(DB::raw("FIELD(id, " . $ids->implode(', ') . ") DESC"))
+            ->withCount(['questionAnswerTypes','questionAnswersRahjoos'=>function($query)use($rahjoo){
+                $query->where('id',$rahjoo->id);
+            }])->orderByRaw(DB::raw("FIELD(id, " . $ids->implode(', ') . ") DESC"))
             ->when($request->filled('lock'), function (Builder $builder) use ($request) {
                 $builder->when($request->lock == "locked", function (Builder $builder) use ($request) {
                     $builder->locked();
