8fe7b69496a7e5d5aa4f387ad7c9edbd3df74a00
alikeshtkar <alikeshtkar262@gmail.com>
2023-02-09 19:16:47 +0330

    push

--- app/Http/Resources/V1/User/UserResource.php
+++ app/Http/Resources/V1/User/UserResource.php
@@ -6,6 +6,7 @@ use App\Enums\UserStatus;
 use App\Http\Resources\V1\Personnel\PersonnelResource;
 use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 
 class UserResource extends JsonResource
@@ -23,7 +24,7 @@ class UserResource extends JsonResource
             $collection->put('status', $status->key)->put('status_translated', $status->description);
         })->when($this->resource->originalIsEquivalent('birthdate'), function (Collection $collection) {
             $collection->put('birthdate', jalaliFormat($this->resource->birthdate, User::BIRTHDATE_VALIDATION_FORMAT))
-                ->put('age', $this->resource->birthdate->age);
+                ->put('age', Carbon::now()->diffInYears($this->resource->birthdate));
         })->when($this->resource->originalIsEquivalent('grade_name'), function (Collection $collection) {
             $collection->put('grade_name', $this->resource->grade_name);
         })->when($this->resource->originalIsEquivalent('city_name'), function (Collection $collection) {
