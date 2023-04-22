a7de7258936f8ab782f3a5537edb3516385d10b3
mralikeshtkar <alikeshtkar262@gmail.com>
2023-04-22 17:48:24 +0330

    123

--- app/Http/Controllers/V1/Api/Discount/ApiDiscountController.php
+++ app/Http/Controllers/V1/Api/Discount/ApiDiscountController.php
@@ -57,15 +57,13 @@ class ApiDiscountController extends ApiBaseController
     }
 
     /**
-     * Get a province with cities.
-     *
      * @OA\Get(
      *     path="/discounts/{id}",
      *     summary="دریافت اطلاعات تخفیف",
-     *     description="دریافت استان به همراه شهرها",
-     *     tags={"استان"},
+     *     description="",
+     *     tags={"تخفیف"},
      *     @OA\Parameter(
-     *         description="شناسه استان",
+     *         description="شناسه تخفیف",
      *         in="path",
      *         name="id",
      *         required=true,
@@ -74,11 +72,6 @@ class ApiDiscountController extends ApiBaseController
      *     @OA\Response(
      *         response=200,
      *         description="OK",
-     *         @OA\JsonContent()
-     *     ),
-     *     @OA\Response(
-     *         response=404,
-     *         description="شناسه استان معتبر نمیباشد",
      *         @OA\JsonContent()
      *     )
      * )
@@ -89,8 +82,60 @@ class ApiDiscountController extends ApiBaseController
     }
 
     /**
-     * @param Request $request
-     * @return JsonResponse
+     * @OA\Post(
+     *     path="/discounts",
+     *     summary="ثبت تخفیف",
+     *     description="",
+     *     tags={"تخفیف"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"code","amount"},
+     *                 @OA\Property(
+     *                     property="code",
+     *                     type="string",
+     *                     description="کد"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="is_percent",
+     *                     type="number",
+     *                     description="آیا درصد است یا خیر (بصورت 0 یا 1)"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="amount",
+     *                     type="string",
+     *                     description="میزان تخفیف"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="enable_at",
+     *                     type="string",
+     *                     description="فعال بودن تخفیف از تاریخ - به فرمت Y/m/d H:i:s"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="expire_at",
+     *                     type="string",
+     *                     description="فعال بودن تخفیف تا تاریخ - به فرمت Y/m/d H:i:s"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="usage_limitation",
+     *                     type="string",
+     *                     description="محدودیت تعداد استفاده"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="status",
+     *                     type="string",
+     *                     description="وضعیت : Active,Inactive"
+     *                 ),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="عملیات موفق",
+     *         @OA\JsonContent()
+     *     ),
+     * )
      * @throws Exception
      */
     public function store(Request $request)
@@ -99,9 +144,74 @@ class ApiDiscountController extends ApiBaseController
     }
 
     /**
-     * @param Request $request
-     * @param $discount
-     * @return JsonResponse
+     * @OA\Post(
+     *     path="/discounts/{id}",
+     *     summary="بروزرسانی تخفیف",
+     *     description="",
+     *     tags={"تخفیف"},
+     *     @OA\Parameter(
+     *         description="شناسه تخفیف",
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="number"),
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"_method","code","amount"},
+     *                 @OA\Property(
+     *                     property="_method",
+     *                     type="string",
+     *                     default="put",
+     *                     enum={"put"},
+     *                     description="این مقدار باید بصورت ثابت شود",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="code",
+     *                     type="string",
+     *                     description="کد"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="is_percent",
+     *                     type="number",
+     *                     description="آیا درصد است یا خیر (بصورت 0 یا 1)"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="amount",
+     *                     type="string",
+     *                     description="میزان تخفیف"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="enable_at",
+     *                     type="string",
+     *                     description="فعال بودن تخفیف از تاریخ - به فرمت Y/m/d H:i:s"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="expire_at",
+     *                     type="string",
+     *                     description="فعال بودن تخفیف تا تاریخ - به فرمت Y/m/d H:i:s"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="usage_limitation",
+     *                     type="string",
+     *                     description="محدودیت تعداد استفاده"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="status",
+     *                     type="string",
+     *                     description="وضعیت : Active,Inactive"
+     *                 ),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="عملیات موفق",
+     *         @OA\JsonContent()
+     *     ),
+     * )
      * @throws Exception
      */
     public function update(Request $request, $discount)
@@ -109,6 +219,26 @@ class ApiDiscountController extends ApiBaseController
         return $this->discountService->update($request, $discount);
     }
 
+    /**
+     * @OA\Delete (
+     *     path="/discounts/{id}",
+     *     summary="حذف تخفیف",
+     *     description="",
+     *     tags={"تخفیف"},
+     *     @OA\Parameter(
+     *         description="شناسه تخفیف",
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="number"),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent()
+     *     )
+     * )
+     */
     public function destroy(Request $request, $discount)
     {
         return $this->discountService->destroy($request, $discount);
